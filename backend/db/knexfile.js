1ca644a2d77a83e53c0b7e2a4bdd826f4c8f7932
David Shipuk <dshipuk@yahoo.com>
2022-09-27 15:35:39 -0700

    installed knex -g wasn't being recognized in terminal init knex and created db file still need too setup db connection

---
+++ backend/db/knexfile.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
