c4235191e973260655cbe988c2d368d672a3d9a3
David Shipuk <dshipuk@yahoo.com>
2022-10-27 15:19:55 -0700

    created a function to restrict access to certain webpages if they do not have a valid token in the auth-middleware

--- backend/api/auth/auth-middleware.js
+++ backend/api/auth/auth-middleware.js
@@ -1,3 +1,22 @@
+const { JWT_SECRET } = require("../secrets");
+const jwt = require("jsonwebtoken");
+
+const restricted = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return next({ status: 401, message: "Token Missing" });
+  }
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      return next({ status: 401, message: "Token Invalid" });
+    } else {
+      req.decodedToken = decodedToken;
+      next();
+    }
+  });
+};
+
 const validateRegistration = (req, res, next) => {
   if (1 == 1) {
     next();
